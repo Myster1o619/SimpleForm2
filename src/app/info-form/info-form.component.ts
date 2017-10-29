6625b89c5b9fbddc09a3b1f1c824ae813d0006f5
Andrew Green <myster1o619@hotmail.com>
2017-10-29 07:13:34 +0200

    complete basic model driven form

--- src/app/info-form/info-form.component.ts
+++ src/app/info-form/info-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'info-form',
@@ -15,16 +15,48 @@ export class InfoFormComponent implements OnInit {
   ];
 
   myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
 
   ngOnInit() {
-    this.myform = new FormGroup({
-      name: new FormGroup({ 
-        firstName: new FormControl(),
-        lastName: new FormControl(),
+    this.createFormControls();
+    this.createForm();
+    
+  }
+  createFormControls() {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*"),
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]);
+    this.language = new FormControl('', Validators.required);
+  }
+
+  createForm() {
+    this.myform = new FormGroup({ 
+      name: new FormGroup ({ 
+        firstName: this.firstName,
+        lastName: this.lastName,
       }),
-      email: new FormControl(),
-      password: new FormControl(),
-      language: new FormControl(),
+      email: this.email,
+      password: this.password,
+      language: this.language,
     });
+  }
+
+  onSubmit() {
+    if (this.myform.valid) {
+      console.log("Form has been submitted");
+      this.myform.reset();
+    }
+    
   }
 }
